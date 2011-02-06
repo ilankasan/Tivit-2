cf50b1dfd7daadb7c8ddcd4c09c4ef1861f4cbc8
ilankasan <ilan.kasan1@gmail.com>
2011-02-06 13:16:55 -0800

    before email invite 10

---
+++ Copy of g.bat
@@ -0,0 +1,24 @@
+git add .
+git commit -m "Added user tivities - end of chapter 11"
+git checkout master
+<<<<<<< HEAD
+$ git merge user-microposts
+=======
+git merge user-microposts
+>>>>>>> user-tasks
+
+git push
+git push heroku
+
+
+
+#git add .
+#git commit -am "complete sign up end of chapter 8"
+#git checkout master
+#git merge signing-up
+
+
+
+
+#git push
+ 
--- g.bat
+++ g.bat
@@ -1,24 +1,4 @@
 git add .
-git commit -m "Added user tivities - end of chapter 11"
-git checkout master
-<<<<<<< HEAD
-$ git merge user-microposts
-=======
-git merge user-microposts
->>>>>>> user-tasks
-
-git push
-git push heroku
-
-
-
-#git add .
-#git commit -am "complete sign up end of chapter 8"
-#git checkout master
-#git merge signing-up
-
-
-
-
-#git push
- 
+git commit -m "before email invite 10"
+git rm GemFile.lock
+git push heroku 
