ce93154cc10f0320bb43726057b2d6bc9e505d88
ilankasan <ilan.kasan1@gmail.com>
2011-03-09 01:21:51 -0800

    Yaniv and Ilan changes: description 1024 chars and added summary field in activity 3

--- db/migrate/20110309075012_add_summary_to_activities.rb
+++ db/migrate/20110309075012_add_summary_to_activities.rb
@@ -1,6 +1,7 @@
 class AddSummaryToActivities < ActiveRecord::Migration
   def self.up
-    add_column :activities, :summary, :text, :limit => 1024
+  #  add_column :activities, :summary, :text, :limit => 1024
+   add_column :activities, :summary, :text
   end
 
   def self.down
