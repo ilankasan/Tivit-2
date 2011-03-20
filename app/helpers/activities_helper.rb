228fc7bc58383c1eced129c4acb8b59b89089d21
ilankasan <ilan.kasan1@gmail.com>
2011-03-20 08:28:32 -0700

    clean up20-1

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -12,7 +12,7 @@ module ActivitiesHelper
 	  	if(user == nil)
 	#create an inactive user
 			config.debug("--->>> creating a skeloton user for email: "+email)
-			params = {:name => "not active",:email => email, :password => "111111",:password_confirmation => "111111"}
+			params = {:name => "not active",:email => email, :password => "234F5(67890GDFSF",:password_confirmation => "111111"}
 			
 	#ilan: using temporary password. Deactivated user should not have passwords	
 			puts "------------------ creating new user: " + email 
--- db/migrate/20110127075729_create_users.rb
+++ db/migrate/20110127075729_create_users.rb
@@ -3,9 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-
       t.timestamps
     end
+    add_index :users, :email, :unique => true
+    
   end
 
   def self.down
