f83e27c7e48a3d1f4d157336799f05feb36bbfae
ilankasan <ilan.kasan1@gmail.com>
2011-03-22 23:28:03 -0700

    upload docs -1

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 class Document < ActiveRecord::Base
 	
-	  belongs_to :activitity_documents  
+	 
+	  belongs_to :activitity_document, :foreign_key => "document_id" 
 	  attr_accessible :note, :version
  
 	  mount_uploader :name, TivitDocUploader 
