9a86a89be66aa7315d5281e5240b1e867db105ac
ilankasan <ilan.kasan1@gmail.com>
2011-09-20 22:50:38 -0700

    9-20-11-c

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,19 +2,64 @@ class PagesController < ApplicationController
   
   def home
     @title = "Home"
-    if signed_in?
-           
-# returns al;l activities that are not completed abd i either own or i have a a tivit (tivit can be completed or not)
+    if signed_in?      
+# returns all activities that are not completed abd i either own or i have a a tivit (tivit can be completed or not)
    		current_user_id = current_user.get_id.inspect
 
       	sql_activities_i_participate = "SELECT DISTINCT activities.* FROM activities, activities as tivits 
-      				   WHERE NOT activities.status         = 'Completed'  AND activities.activity_type = 'activity' 
-      				   AND (activities.owner_id    = "+current_user_id+"
-      				   OR (tivits.owner_id = "+current_user_id+" AND tivits.parent_id = activities.id))
+      				   WHERE NOT activities.status      = 'Completed'  
+      				   AND activities.activity_type 	= 'activity' 
+      				   AND (activities.owner_id    		= "+current_user_id+"
+      				   OR ( 
+      				   tivits.owner_id 			= "+current_user_id+" 
+      				   AND tivits.parent_id 	= activities.id))
       				   ORDER BY activities.due"
       				           
-      				   
       	@tivits_participate = Activity.find_by_sql(sql_activities_i_participate)
+      	
+      	
+# Return all new activities
+      	sql_new_tivits_old = "SELECT DISTINCT activities.* FROM activities, activities as tivits, tivit_user_statuses 
+      				   WHERE NOT activities.status         = 'Completed' 
+      				   AND activities.activity_type 	   = 'activity' 
+      				   AND   NOT activities.owner_id       = "+current_user_id+" 
+      				   AND tivit_user_statuses.activity_id = activities.id 
+      				   AND tivit_user_statuses.status_id   = 'New' 
+      				   AND tivit_user_statuses.user_id     = "+current_user_id+"
+      				   ORDER BY activities.due"
+      				   
+      				   
+      sql_new_tivits_old2 = "SELECT DISTINCT activities.* FROM activities, activities as tivits, tivit_user_statuses
+      				   WHERE NOT activities.status      = 'Completed'  
+      				   AND activities.activity_type 	= 'activity' 
+      				   AND 
+      				   (    activities.owner_id    		= "+current_user_id+"
+      				   	AND tivit_user_statuses.activity_id = activity.id 
+      				    AND tivit_user_statuses.status_id   = 'New' 
+      				    AND tivit_user_statuses.user_id     = "+current_user_id+"
+      				   
+      				   		OR 
+      				   		(    tivits.owner_id 	= "+current_user_id+" 
+      				   		 AND tivits.parent_id 	= activities.id
+      				   		 AND tivit_user_statuses.activity_id = tivits.id 
+      				   		 AND tivit_user_statuses.status_id   = 'New' 
+      				   		 AND tivit_user_statuses.user_id     = "+current_user_id+"
+      				   		 )
+      				   )
+      				   ORDER BY activities.due"
+	 
+	  sql_new_tivits = "SELECT DISTINCT activities.* FROM activities, activities as tivits, tivit_user_statuses
+      				   WHERE NOT activities.status         = 'Completed'  
+      				   AND activities.activity_type 	   = 'activity' 
+      				   AND tivits.owner_id 				   = "+current_user_id+" 
+      				   AND tivits.parent_id 			   = activities.id
+      				   AND tivit_user_statuses.activity_id = tivits.id 
+      				   AND tivit_user_statuses.status_id   = 'New' 
+      				   AND tivit_user_statuses.user_id     = "+current_user_id+"
+      				   ORDER BY activities.due"
+	 
+	 
+	  @tivits_new          = Activity.find_by_sql(sql_new_tivits)
      end
   end
 
@@ -42,8 +87,9 @@ def home_old
 	 
 	  @tivits_new          = Activity.find_by_sql(sql_new_tivits)
       #@tivits_completed       = current_user.activities.where("status = 'Completed' AND completed_at > ?",)
-      @tivits_completed       = current_user.activities.where("status = 'Completed'")
-     end
+      #@tivits_completed       = current_user.activities.where("status = 'Completed'")
+      
+      end
   end
 
   
