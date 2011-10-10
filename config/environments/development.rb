903ef82131cae6e0383cfe1e046ba59511b6a60e
ilankasan <ilan.kasan1@gmail.com>
2011-10-10 00:05:07 -0700

    10-9-11 - Added Devise v1d

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,7 @@ cache_classes = false
   # config/environments/development.rb
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
