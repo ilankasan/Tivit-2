42cc7330337f63dd101451b984aee167271aa916
ilankasan <ilan.kasan1@gmail.com>
2011-10-09 23:59:47 -0700

    10-9-11 - Added Devise v1c

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,9 +51,12 @@ FirstApp::Application.configure do
   #config.port	   = nil 
   
 
+  config.action_mailer.default_url_options = { :host => 'tiviti.heroku.com' }
+  
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
 	  :address        => 'smtp.gmail.com',
