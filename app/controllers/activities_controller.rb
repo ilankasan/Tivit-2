bb00dfb56a2a928a39e44fb0b6da0a3938909d05
ilankasan <ilan.kasan1@gmail.com>
2011-10-23 20:17:42 -0700

    12-23-11a1

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -78,8 +78,10 @@ class ActivitiesController < ApplicationController
   def remove_tivit
 	puts "Remove Tivit"
 	@tivit = Activity.find(params[:id])
+	@activity = @tivit.get_parent
 	@tivit.destroy
-	redirect_back_or root_path
+	#redirect_back_or root_path
+	redirect_to @activity
       	
   end
   
