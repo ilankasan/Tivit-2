cf194dd38e69f7a100d116ab77438aeb116bb7dc
ilankasan <ilan.kasan1@gmail.com>
2011-10-30 00:01:14 -0700

    10-29-11 reports 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -136,10 +136,13 @@ class PagesController < ApplicationController
      @title = "Product Adoption Dashboard"
      activated         = User.where("is_active = ?", '1').count
      total_users       = User.count
-     non_activated     = total_users - activated 
-     puts "activated = "+activated.to_s+ " non activate = "+non_activated.to_s+ " total = "+total_users.to_s
+     non_activated     = total_users - activated
+     viraly_joined     = User.where("is_active = ? AND clone_email is not null", '1').count
+      
+     #puts "activated = "+activated.to_s+ " non activate = "+non_activated.to_s+ " total = "+total_users.to_s+ " viraly_joined = " + viraly_joined.to_s
+     puts @user_adopt.inspect 
 
-     @user_adopt = {"activated" => activated.to_s, "non_activated" => non_activated.to_s, "total" => total_users.to_s}
+     @user_adopt = {"activated" => activated.to_s, "non_activated" => non_activated.to_s, "total" => total_users.to_s, "viraly_joined" => viraly_joined.to_s}
      #@user_adopt = Hash.new("activated" => activated.to_s, "non_activated" => non_activated.to_s, "total" => total_users.to_s)
   end
 
