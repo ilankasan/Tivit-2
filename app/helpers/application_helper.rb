819ab5bd75425fb7e6595b4f3dfd563f15b654b1
ilankasan <ilan.kasan1@gmail.com>
2012-01-25 21:05:10 -0800

    1-25-12 staging 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,32 @@ module ApplicationHelper
     end
   end
   
+  # Check the client version used to access the application. No versioning distinction yet as it's really not needed at this point. 
+  def client_browser_name
   
+    user_agent = request.env['HTTP_USER_AGENT'].downcase
+     
+    if user_agent =~ /msie/i
+      return "Internet Explorer"
+    elsif user_agent =~ /konqueror/i
+      return  "Konqueror"
+    elsif user_agent =~ /gecko/i
+      return  "Mozilla"
+    elsif user_agent =~ /opera/i
+      return "Opera"
+    elsif user_agent =~ /applewebkit/i
+      return "Safari"
+    else
+      return  "Unknown"
+    end
+  end
+  
+  def client_browser_supported?
+    if (client_browser_name == "Internet Explorer" )
+      return false
+    else
+      return true
+    end
+  end
+                  
 end
