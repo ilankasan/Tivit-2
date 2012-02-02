03608e38ce89a94740bc7ea10995cb909aedb9ee
ilankasan <ilan.kasan1@gmail.com>
2012-02-01 22:02:42 -0800

    1-31-12 autocomplete s 10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ FirstApp::Application.configure do
  config.action_mailer.default_url_options = { :host => 'tiviti-staging.heroku.com' }
  config.action_mailer.asset_host          = "tiviti-staging.heroku.com"
     
+    
   #config.action_mailer.default_url_options = { :host => 'tiviti-s2.heroku.com' }
   #config.action_mailer.asset_host          =            "tiviti-s2.heroku.com"
 
