8aadbcfedd9251cc6f703621477661fa92c2fd01
ilankasan <ilan.kasan1@gmail.com>
2012-02-17 00:33:56 -0800

    2-16-12 s2 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,65 +23,65 @@ class PagesController < ApplicationController
 #<%= select_tag(:filter_id, options_for_select([['On Deck', 1],['Just My Tivits', 2], ['All Unresponded', 3],['All Open', 4]],account_session[:filter_id])) %>
 
 def home
+    puts "HHHHHHHHHHHHHHHHHHHHOOOOOOOOOOOOOOOOOOOOOOOOOMMMMMMMMMMMMMMMMMEEEEEEEEEEEEEEEEEEEE"
     @title = "Home"
     
     puts "Home"
-    #puts "Params = "+params.inspect
-    #if(account_session[:flash_error] != nil && !account_session[:flash_error].empty?)
-      
-      #flash[:failed] = account_session[:flash_error]
-     # puts "flash = "+ flash.inspect 
-     # account_session[:flash_error] = nil
-    #end
-    current_user_id = current_account.user.get_id.inspect
+    puts "HHHHHHHHHHHHHHHHHHHHOOOOOOOOOOOOOOOOOOOOOOOOOMMMMMMMMMMMMMMMMMEEEEEEEEEEEEEEEEEEEE"
+    puts "HHHHHHHHHHHHHHHHHHHHOOOOOOOOOOOOOOOOOOOOOOOOOMMMMMMMMMMMMMMMMMEEEEEEEEEEEEEEEEEEEE"
+    
+   
+    current_user_id = current_account.user.get_id.to_s
     puts "current_user_id = "+current_user_id
-    @title = "Home"
+    puts "Sign in account = "+current_account.sign_in_count.to_s
+    if(current_account.sign_in_count == 1 && !account_session[:show_help] == true )
+      puts "befor render"
+      render 'help'
+      account_session[:show_help] = true
+      #render help_path
+      
+      return
+    else
+
     
     #ccount_session[:filter_id] = "1" if account_session[:filter_id] == nil
      
-    if((account_session[:filter_id]==nil) && (params[:filter_id]==nil))
-      @filter_id = "1"  
-    elsif (params[:filter_id] != nil)
-      @filter_id = params[:filter_id]
-    else
-      @filter_id = account_session[:filter_id]
-    end
-      
-    
-    case @filter_id
-    when ("1") # On Deck
-        @activities_summary             = get_activities_i_participate(current_user_id)
-              
-    when ("2") # Just My tivits
-        puts "Just My tivits"
-       @activities_summary             = get_activities_i_have_open_tivits (current_user_id)
-       
-        
-    when ("3") # All unresponded
-        puts "All unresponded"
-         @activities_summary             = get_activities_i_participate (current_user_id)
-      
-        
-    when ("4") # All open
-        puts "All open"
-        @activities_summary             = get_activities_i_participate (current_user_id)
-    
-          
+      if((account_session[:filter_id]==nil) && (params[:filter_id]==nil))
+        @filter_id = "1"  
+      elsif (params[:filter_id] != nil)
+        @filter_id = params[:filter_id]
       else
-        @activities_summary             = get_activities_i_participate (current_user_id)
-        #not sure we get here.
-        @filter_id = "1"    
+        @filter_id = account_session[:filter_id]
       end
-# Filter only product On Deck (for now)
-    @completed_activities          = get_activities_completed_or_with_completed_tivits(current_user_id)
-  #  puts "----------------    111111111111111111111111   completed activities "
-  #  puts @completed_activities.inspect
         
-    
-    @incoming_activities       = get_activities_with_new_tivit_requests(current_user_id)
-  #  @need_attention_activities = get_need_attention (current_user_id)
-    
-    account_session[:filter_id] = @filter_id
+      
+      case @filter_id
+      when ("1") # On Deck
+          @activities_summary             = get_activities_i_participate(current_user_id)
+                
+      when ("2") # Just My tivits
+          puts "Just My tivits"
+         @activities_summary             = get_activities_i_have_open_tivits (current_user_id)
+         
+      when ("3") # All unresponded
+          puts "All unresponded"
+           @activities_summary            = get_activities_i_participate (current_user_id)
+           
+      when ("4") # All open
+          puts "All open"
+          @activities_summary             = get_activities_i_participate (current_user_id)
+          
+        else
+          @activities_summary             = get_activities_i_participate (current_user_id)
+          @filter_id = "1"    
+        end
+  # Filter only product On Deck (for now)
+      @completed_activities          = get_activities_completed_or_with_completed_tivits(current_user_id)
+      @incoming_activities           = get_activities_with_new_tivit_requests(current_user_id)
+      @need_attention_activities     = get_need_attention (current_user_id)
+      
+      account_session[:filter_id] = @filter_id
+     end
   end
 
 
