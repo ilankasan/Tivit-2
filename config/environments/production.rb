bfb9557de78ec89ddf07fa8650e77b418ea32261
ilankasan <ilan.kasan1@gmail.com>
2012-05-06 17:41:11 -0700

    5-6-12 completed ACTIVITY LOGIV  S2 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ FirstApp::Application.configure do
     
  config.action_mailer.default_url_options = { :host => 'tiviti-s2.heroku.com' }
  config.action_mailer.asset_host          =            "tiviti-s2.heroku.com"
-#
+
 
  #config.action_mailer.default_url_options = { :host => 'tiviti.heroku.com' }
  #config.action_mailer.asset_host          =            "tiviti.heroku.com"
